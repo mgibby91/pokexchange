b211fba237a49b2a6cb316ac8634c5229a8f673c
Matthew Gibson <mattgibson6@gmail.com>
2020-08-22 16:07:06 +0000

    first draft of creating all 6 tables

---
+++ db/schema/02_listings.sql
@@ -0,0 +1,22 @@
+
+DROP TABLE IF EXISTS listings
+CASCADE;
+
+CREATE TABLE listings
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL DEFAULT 0,
+  condition VARCHAR(255) NOT NULL,
+  time_posted TIMESTAMP NOT NULL,
+  description TEXT,
+  location VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  active BOOLEAN NOT NULL DEFAULT TRUE
+  -- simply active to start db
+);
+
+
---
+++ db/schema/06_categories.sql
@@ -0,0 +1,9 @@
+
+DROP TABLE IF EXISTS categories
+CASCADE;
+
+CREATE TABLE categories
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
