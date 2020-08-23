d4e765021f86d21671c270979d92c742cd4b1e9b
potatolight <shuguangbai@gmail.com>
2020-08-23 22:39:10 +0000

    add userlisting, create_listing, favorate_listing

--- routes/widgets.js
+++ routes/widgets.js
@@ -7,21 +7,94 @@
 
 const express = require('express');
 const router  = express.Router();
+const {
+  getAllListingsByMostRecent,
+  getAllListingsByUserID,
+  getAllListingsByCategory,
+  getAllListingsByPriceRange,
+  getAllListingsUserFavourited,
+  addCards
+} = require('../lib/listing-queries');
+const { get } = require('./users');
 
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-  return router;
-};
+//show the user-lists
+router.get('/creat_list', (req,res) => {
+  const userId = req.session.user_id
+ getAllListingsByUserID(userId)
+  .then((uresults) => {
+    console.log ("show the userid list: ", uresults);
+    res.render('test1', {uresults})
+  })
+})
+
+//show the user-favout list
+router.get('/favourites', (req, res) => {
+  const userId = req.session.user_id
+  getAllListingsUserFavourited(userId)
+  .then((fresults) => {
+    console.log ("show the favorate list: ", fresults)
+    res.render('test2', {fresults})
+  })
+})
+
+//user add new products
+router.post('/', (req, res) => {
+  const userId = req.session.user_id;
+  addCards(({...req.body, owner_id: userId}))
+   .then((cards) => {
+     console.log(cards)
+
+     res.send(cards)
+   })
+})
+
+//show all products by time
+router.get('/', (req, res) => {
+  getAllListingsByMostRecent()
+    .then((results) => {
+      res.render("test", {results})
+    })
+})
+
+
+
+
+
+
+// router.get('/category')
+
+// router.get('/', (req,res) => {
+//   const userId = req.session.userId;
+//   if (!userId) {
+//     res.send('please login first')
+//     return;
+//   }
+//   getAllListingsByUserID(userId)
+//    .then((results) => {
+//      console.log (results)
+//      res.render('test1', {results})
+//    })
+//    .catch(e => {
+//     res.send(e)
+//   });
+// })
+
+module.exports = router;
+
+// module.exports = (db) => {
+//   router.get("/", (req, res) => {
+//     let query = `SELECT * FROM widgets`;
+//     console.log(query);
+//     db.query(query)
+//       .then(data => {
+//         const widgets = data.rows;
+//         res.json({ widgets });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+//   });
+//   return router;
+// };
