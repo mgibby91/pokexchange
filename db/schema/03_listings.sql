49b296cbb755c3be837be2afd9c1104c62691322
Matthew Gibson <mattgibson6@gmail.com>
2020-08-23 03:28:41 +0000

    DB connection code and first few listings queries complete

--- db/schema/03_listings.sql
+++ db/schema/03_listings.sql
@@ -10,7 +10,7 @@ CREATE TABLE listings
   price INTEGER NOT NULL DEFAULT 0,
   condition VARCHAR(255) NOT NULL,
   time_posted TIMESTAMP NOT NULL,
-  description TEXT,
+  description TEXT NOT NULL,
   city VARCHAR(255) NOT NULL,
   country VARCHAR(255) NOT NULL,
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
