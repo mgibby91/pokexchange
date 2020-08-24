8e987e01dd17b9f60ed86c5be72632433358b913
Matthew Gibson <mattgibson6@gmail.com>
2020-08-24 14:46:55 +0000

    modify all listings queries to include categories

--- bin/test.sql
+++ bin/test.sql
@@ -1,4 +1,6 @@
 
-SELECT *
+SELECT *, category.name as category
 FROM listings
-ORDER BY time_posted DESC;
+  JOIN categories ON category_id = categories.id
+WHERE price > 1 AND price < 1000
+ORDER BY price ASC;
