e881c6862ccfa90bd5afde66a9c50851ec7d2559
Matthew Gibson <mattgibson6@gmail.com>
2020-08-24 19:52:18 +0000

    add queries for getting listings by top favourited and search filtering

--- bin/test.sql
+++ bin/test.sql
@@ -1,5 +1,7 @@
 
-SELECT *
-FROM messages
-WHERE (buyer_id = 2 OR seller_id = 2) AND listing_id = 16
-ORDER BY time_sent;
+SELECT listings.*, count(favourites.id) as number_of_favourites
+FROM listings
+  JOIN favourites ON listing_id = listings.id
+GROUP BY listings.id
+ORDER BY number_of_favourites DESC
+LIMIT 3;
