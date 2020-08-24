afdcd0491f61af2a0bef80d63881b80f428c4f90
Matthew Gibson <mattgibson6@gmail.com>
2020-08-24 23:19:01 +0000

    add helper function to lib dir to add array of multiple image_urls to listings query objects

--- bin/test.sql
+++ bin/test.sql
@@ -1,4 +1,11 @@
 
-SELECT *
-FROM users
-WHERE username = 'mdaniely9';
+-- SELECT listings.id as listing_id, categories.name as category, listings.title as title
+-- FROM listings
+--   JOIN users ON listings.user_id = users.id
+--   JOIN categories ON category_id = categories.id
+-- WHERE users.id = 3
+-- ORDER BY time_posted DESC;
+
+SELECT listing_id as listing_id, image_url
+FROM images
+ORDER BY listing_id;
