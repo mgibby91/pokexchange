926b63649ffcb9d071a109d700e7736b4691b73f
Matthew Gibson <mattgibson6@gmail.com>
2020-08-24 20:04:13 +0000

    add users queries file in db and created 2 functions

--- bin/test.sql
+++ bin/test.sql
@@ -1,7 +1,4 @@
 
-SELECT listings.*, count(favourites.id) as number_of_favourites
-FROM listings
-  JOIN favourites ON listing_id = listings.id
-GROUP BY listings.id
-ORDER BY number_of_favourites DESC
-LIMIT 3;
+SELECT *
+FROM users
+WHERE username = 'mdaniely9';
