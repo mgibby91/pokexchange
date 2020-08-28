9720520bda05255bdacda8b1fb3b81f80df28e31
Matthew Gibson <mattgibson6@gmail.com>
2020-08-28 18:54:52 +0000

    c r

--- db/seeds/06_messages.sql
+++ db/seeds/06_messages.sql
@@ -5,11 +5,7 @@ values
 insert into messages
   (buyer_id, seller_id, text_body, time_sent, written_by, active, listing_id)
 values
-<<<<<<< HEAD
-  (2, 1, 'Yes you may, friendo! But the price has increased to $10,000 as it is currently evolved to Sylveon 🙈🙊 ', '2020-08-16T07:39:19Z', 1, true, 1);
-=======
   (2, 1, 'Yes you may, friendo! But the price has increased to $10,000 as it is currently evolved to Sylveon 🙈🙊', '2020-08-16T07:39:19Z', 1, true, 1);
->>>>>>> 853d9bb8b7f89ece0eefc4513f997df53315c82c
 insert into messages
   (buyer_id, seller_id, text_body, time_sent, written_by, active, listing_id)
 values
